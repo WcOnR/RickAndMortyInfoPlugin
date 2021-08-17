cc0fb2f246a7725be0830d12d35cadb54c1edbff
Danil Loik <wconr@mail.ru>
2021-08-17 22:28:30 +0300

    Create empty project

---
+++ Source/RMInfoPlugin/RMInfoPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RMInfoPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RMINFOPLUGIN_API ARMInfoPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
